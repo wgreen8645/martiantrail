1bb1c58a2a08d3483dcc8e7b2df813f6d92532d1
wgreen8645 <william.green8645@gmail.com>
2018-05-17 21:57:50 -0500

    update

--- js/Game.js
+++ js/Game.js
@@ -140,9 +140,5 @@ MarsH.Game.resumeJourney = function() {
   this.step();
 };
 
-if(this.ship.distance == 200) {
-	this.ui.notify('You\'re halfway to your destination. The war still ensues on Earth')
-};
-
 //init game
 MarsH.Game.init();
